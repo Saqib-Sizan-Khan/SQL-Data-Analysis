86a4cd6540d907ec90c84809d69a111941faccc6
Saqib Sizan Khan <ssk123098@gmail.com>
2024-08-01 00:03:38 +0600

    several analysis query added

--- EDA.sql
+++ EDA.sql
@@ -23,25 +23,59 @@ GROUP BY the_year
 ORDER BY the_year DESC;
 
 -- Company yearly laid offs
-SELECT company, YEAR(`date`) AS laid_off_year, SUM(total_laid_off) AS laid_off
+SELECT company, YEAR(`date`) AS years, SUM(total_laid_off) AS laid_off
 FROM layoffs_staging2
-GROUP BY YEAR(`date`), company
+GROUP BY years, company
 HAVING laid_off != 0
-ORDER BY company, laid_off_year;
+ORDER BY laid_off DESC;
 
 -- Industry yearly laid offs
-SELECT industry, YEAR(`date`) AS laid_off_year, SUM(total_laid_off) AS laid_off
+SELECT industry, YEAR(`date`) AS years, SUM(total_laid_off) AS laid_off
 FROM layoffs_staging2
-GROUP BY YEAR(`date`), industry
+GROUP BY years, industry
 HAVING laid_off != 0
-ORDER BY industry, laid_off_year;
+ORDER BY laid_off DESC;
 
 -- Country yearly laid offs
-SELECT country, YEAR(`date`) AS laid_off_year, SUM(total_laid_off) AS laid_off
+SELECT country, YEAR(`date`) AS years, SUM(total_laid_off) AS laid_off
 FROM layoffs_staging2
-GROUP BY YEAR(`date`), country
+GROUP BY years, country
 HAVING laid_off != 0
-ORDER BY country, laid_off_year;
+ORDER BY laid_off DESC;
+
+SELECT YEAR(`date`) AS `Year`, MONTH(`date`) AS `Month`, MONTHNAME(`date`) AS `MonthName`, SUM(total_laid_off) AS laid_off
+FROM layoffs_staging2
+GROUP BY `Year`, `Month`, `MonthName`
+HAVING `Year` IS NOT NULL
+ORDER BY `Year`, `Month`;
+
+WITH Rolling_total AS
+(
+	SELECT SUBSTRING(`date`, 1, 7) AS `Month`, SUM(total_laid_off) AS laid_off
+	FROM layoffs_staging2
+	GROUP BY `Month`
+	HAVING `Month` IS NOT NULL
+	ORDER BY `Month`
+)
+SELECT `Month`, laid_off, SUM(laid_off) OVER(ORDER BY `Month`) AS rolling_total
+FROM Rolling_total;
+
+-- Company ranking by yearly laid offs
+WITH Year_laid_offs AS
+(
+	SELECT company, YEAR(`date`) AS years, SUM(total_laid_off) AS laid_off
+	FROM layoffs_staging2
+	GROUP BY years, company
+	HAVING laid_off != 0 AND years IS NOT NULL
+	ORDER BY laid_off DESC
+), Comapny_ranking As
+(
+	SELECT *, DENSE_RANK() OVER(PARTITION BY years ORDER BY laid_off DESC) AS Ranking
+	FROM Year_laid_offs
+)
+SELECT *
+FROM Comapny_ranking
+WHERE Ranking <= 10;
 
 
 
